7f369a1f9300b5df5a9b4aae1f1c3ad33d0f9796
Felipe Lemos <felipewesley69@gmail.com>
2021-09-23 09:35:19 -0300

    adicinado mais produtos no arrey

--- src/app/promo/promo.component.ts
+++ src/app/promo/promo.component.ts
@@ -29,6 +29,33 @@ export class PromoComponent implements OnInit {
       totalComents: 3,
       store: 'Americanas',
       timePost: '22 minutos'
+    },
+    {
+      linkImg: 'https://cdn.gatry.com/gatry-static/promocao/imagem_url/790566f43f57a453d8543e041cdcde4d.jpg',
+      titleProduct: 'Cadeira Gamer Alpha Gamer Zeta Black Red - AGZETA-BK-R',
+      value: 849.95,
+      fistComent: '',
+      totalComents: 5,
+      store: 'Kabun',
+      timePost: '25 minutos'
+    },
+    {
+      linkImg: 'https://cdn.gatry.com/gatry-static/promocao/imagem/995122240e7c1c43aefd8b2589c2523d.jpg',
+      titleProduct: 'Bicicleta Aro 29 TK 7.0 Mountain Bike Vermelha, Track Bikes',
+      value: 1549,
+      fistComent: '',
+      totalComents: 3,
+      store: 'Americanas',
+      timePost: '22 minutos'
+    },
+    {
+      linkImg: 'https://cdn.gatry.com/gatry-static/promocao/imagem/a4a6ee16ee3fe49c195b552e8af0d09e.png',
+      titleProduct: 'Smartphone ASUS Zenfone Max Pro M2 6GB RAM, 64GB, Black Saphire',
+      value: 899.99,
+      fistComent: 'Menor preço já registrado',
+      totalComents: 9,
+      store: 'Americanas',
+      timePost: '11 horas'
     }
   ];
 }
